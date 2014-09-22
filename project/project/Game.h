d5213aeced78b7773db2536fd7c65b9fab3afed8
Freaky <jan.schmitter@gmail.com>
2014-09-22 22:05:37 +0200

    Basic movement of the circle

---
+++ project/project/Game.h
@@ -0,0 +1,30 @@
+// ====================================================================================================
+// Game Class
+// ====================================================================================================
+#ifndef H_GAME
+#define H_GAME
+
+class Game
+{
+public:
+	Game();
+	void Run();
+
+private:
+	void ProcessEvents();
+	void Update();
+	void Render();
+
+	void HandlePlayerInput(sf::Keyboard::Key key, bool isPressed);
+
+private:
+	bool mIsMovingUp;
+	bool mIsMovingDown;
+	bool mIsMovingLeft;
+	bool mIsMovingRight;
+
+	sf::RenderWindow mWindow;
+	sf::CircleShape mPlayer;
+};
+
+#endif
--- project/project/Includes.h
+++ project/project/Includes.h
@@ -12,3 +12,8 @@
 #pragma comment(lib, "sfml-graphics.lib")
 //#pragma comment(lib, "sfml-audio.lib")
 //#pragma comment(lib, "sfml-network.lib")
+
+// ====================================================================================================
+// Most important classes
+// ====================================================================================================
+#include "Game.h"
