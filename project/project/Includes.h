7a657a857afe3d8f76662af6320a42657176359a
Freaky <jan.schmitter@gmail.com>
2014-09-22 21:51:16 +0200

    Fixed Project, Book example now working

--- project/project/Includes.h
+++ project/project/Includes.h
@@ -6,4 +6,9 @@
 // ====================================================================================================
 // Libraries
 // ====================================================================================================
+#pragma comment(lib, "sfml-main.lib")
+#pragma comment(lib, "sfml-system.lib")
+#pragma comment(lib, "sfml-window.lib")
 #pragma comment(lib, "sfml-graphics.lib")
+//#pragma comment(lib, "sfml-audio.lib")
+//#pragma comment(lib, "sfml-network.lib")
