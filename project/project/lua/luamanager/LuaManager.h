82d011259c133741917d6719325957fd3e0b5992
Freaky <jan.schmitter@gmail.com>
2014-09-27 23:54:37 +0200

    Fully added all Lua files

---
+++ project/project/lua/luamanager/LuaManager.h
@@ -0,0 +1,87 @@
+// ====================================================================================================
+// Lua Manager
+// ====================================================================================================
+#ifndef H_LUA_MANAGER
+#define H_LUA_MANAGER
+
+class LuaManager
+{
+public:
+	// Main
+	LuaManager();
+	void Init();
+	lua_State* State() { return m_pState; }
+	bool LuaManager::IncludeFile(char* sPath);						// nothrow, uses ProtectedCall
+
+	// Error Handling
+	bool Success();
+	void CheckForLuaErrors(int iErrorIndex = 0);
+	void PrintErrorMessage(char* sErrorMsg, bool bDoNotTriggerLuaError = false, bool bDoNotCreateTraceback = false);
+
+	// Calling
+	void Call(int iArgCount = 0, int iResultCount = 0);				// can throw an exception! make sure to catch it
+	bool ProtectedCall(int iArgCount = 0, int iResultCount = 0);	// calls Call() but catches itself. return value = success?
+
+	// Get*
+	void GetGlobal(char* sName);
+	char* GetString(int iStackIndex = -1);
+	double GetNumber(int iStackIndex = -1);
+	bool GetBool(int iStackIndex = -1);
+	void GetTypeName(int iStackIndex);
+
+	// Is*
+	bool IsNil(int iStackIndex = -1);
+	bool IsString(int iStackIndex = -1);
+	bool IsNumber(int iStackIndex = -1);
+	bool IsBool(int iStackIndex = -1);
+	bool IsTable(int iStackIndex = -1);
+	bool IsUserData(int iStackIndex = -1);
+
+	// Push*
+	void PushString(char* sString);
+	void PushNil();
+	void PushNumber(double iNumber);
+	void PushBool(bool bBool);
+	void PushValue(int iStackIndex = -1);
+
+	// Tables
+	bool GetField(char* sKey, int iStackIndex = -1);
+	void SetField(char* sKey, int iStackIndex = -1);
+
+	// Stack Operations
+	int Top(); // stack count [argument count]
+	void Pop(int iStackIndex = -1);
+	void SetTop(int iStackIndex);
+	void Remove(int iStackIndex = -1);
+
+	// Debug Functions
+	lua_Debug GetDebugInformation();
+	char* DebugGetFilename();
+	char* DebugGetPath();
+	
+private:
+	lua_State* m_pState;
+	bool m_bSuccess;
+};
+
+// ====================================================================================================
+// Error Handling
+// ====================================================================================================
+int LuaPanicHandler(lua_State* L);
+
+struct LuaException : public std::exception
+{
+	std::string m_sError;
+	std::string m_sTraceback;
+	LuaException(std::string err) : m_sError(err) {}
+	LuaException(std::string err, std::string trace) : m_sError(err), m_sTraceback(trace) {}
+	~LuaException() throw () {}
+
+	const char* what() const throw() { return m_sError.c_str(); }
+	std::string error() { return m_sError; }
+	std::string traceback() { return m_sTraceback;  }
+};
+
+extern LuaManager* lua;
+
+#endif
